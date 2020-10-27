b97dc8b7281f42990564242ba31b99f4dad7d8be
Pete Lindsell <pete.lindsell@gmail.com>
2020-10-27 23:48:08 +0000

    add config

--- bin/my-pipeline.ts
+++ bin/my-pipeline.ts
@@ -4,4 +4,11 @@ import * as cdk from '@aws-cdk/core';
 import { MyPipelineStack } from '../lib/my-pipeline-stack';
 
 const app = new cdk.App();
-new MyPipelineStack(app, 'MyPipelineStack');
+new MyPipelineStack(app, 'PipelineStack', {
+  env: {
+    account: '453861593793',
+    region: 'eu-west-1',
+  }
+});
+
+app.synth();
--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -1,9 +1,38 @@
-import * as cdk from '@aws-cdk/core';
+import { Stack, StackProps, Construct, SecretValue } from '@aws-cdk/core';
+import { CdkPipeline, SimpleSynthAction } from '@aws-cdk/pipelines';
 
-export class MyPipelineStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+
+export class MyPipelineStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const sourceArtifact = new codepipeline.Artifact();
+    const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+    const pipeline = new CdkPipeline(this, 'Pipeline', {
+      pipelineName: 'MyAppPipeline',
+      cloudAssemblyArtifact,
+
+      sourceAction: new codepipeline_actions.GitHubSourceAction({
+        actionName: 'GitHub',
+        output: sourceArtifact,
+        oauthToken: SecretValue.secretsManager('my-pipeline'),
+        trigger: codepipeline_actions.GitHubTrigger.POLL,
+        // Replace these with your actual GitHub project info
+        owner: 'PeteLindsell',
+        repo: 'my-pipeline',
+      }),
+
+      synthAction: SimpleSynthAction.standardNpmSynth({
+        sourceArtifact,
+        cloudAssemblyArtifact,
+
+        // Use this if you need a build step (if you're not using ts-node
+        // or if you have TypeScript Lambdas that need to be compiled).
+        buildCommand: 'npm run build',
+      }),
+    });
   }
 }
